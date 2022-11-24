3f23497d8cc1155eb8ef7b84302e293a7b1e5370
Ruan Diego Farias <69852934+Ruan-Dieg0@users.noreply.github.com>
2022-11-24 00:47:53 +0000

    Acesso ao banco de dados

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #Importar o arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
 ruan = Pessoa(1, "Ruan Diego")
 print(ruan)
+
+#Mostrar só o nome
+print(ruan.nome)
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Carregar uma lista que veio do banco de dados
+pessoas = pessoaDAO.getAll()
+for pessoa in pessoas:
+  print(pessoa)
 
 
 
